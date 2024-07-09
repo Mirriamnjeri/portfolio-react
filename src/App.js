5b6217cbd99d4d6cfc167229539f3cbb3e07b889
mirriam njeri <njerimirriam456@gmail.com>
2024-07-09 23:17:59 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='heading'>HELLO I AM MIRRIAM</h1>
+      <p className='paragraph'> My name is Migunamiguna. I like coding and politics. I enjoy learning new things!</p>
     </div>
   );
 }
