dd8f9bf191fcaf45bb3ad2e256273c21f2e39eb0
mirriam njeri <njerimirriam456@gmail.com>
2024-07-12 00:14:32 +0300

    message2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-      <h1 className='heading'>HELLO I AM MIRRIAM</h1>
-      <p className='paragraph'> My name is Migunamiguna. I like coding and politics. I enjoy learning new things!</p>
+    <div className="bg-gray-100 p-8">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl font-bold text-blue-500">Welcome to MyApp</h1>
+        <p className="text-lg text-gray-700 mt-4">Your journey to better apps starts here.</p>
+        <button className="mt-6 bg-blue-500 text-white py-2 px-4 rounded">Get Started</button>
+      </div>
     </div>
   );
 }
